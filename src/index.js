dc0954fceeb515392353dc1e3ddc3598d60f2edc
Alena Kharytonchyk <alena.kharytonchyk@outlook.com>
2018-09-12 22:35:20 +0300

    Massives are pure evil!!

--- src/index.js
+++ src/index.js
@@ -4,4 +4,20 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  let count = 0;
+  for (let index = 0; index < preferences.length; index++) {
+    if (preferences[index] -1== index) continue;
+    const element = preferences[index];
+    const element2 = preferences[element-1];
+    const element3 = preferences[element2-1];
+    if (element3-1==index){
+      count+=1
+      preferences[index]=0
+      preferences[element-1]=0
+      preferences[element2-1]=0;
+    }
+
+  }
+return count;
 };
+
